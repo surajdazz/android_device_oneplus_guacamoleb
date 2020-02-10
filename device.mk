def714d1006d7a4c45e8fa63ba05e6cd64595afe
Rick M <spookcity138@gmail.com>
2020-02-10 21:57:19 +0100

    overlay-aosp - > overlay-evolution

--- device.mk
+++ device.mk
@@ -16,10 +16,10 @@ PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-evolution
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-aosp/packages/apps/Snap
+    $(LOCAL_PATH)/overlay-evolution/packages/apps/Snap
 
 # Audio
 PRODUCT_COPY_FILES += \
