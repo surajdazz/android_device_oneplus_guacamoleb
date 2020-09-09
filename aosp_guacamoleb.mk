3e3db9e5116450187b73b7837f15b87a7f7189b1
Joey Huab <joey@evolution-x.org>
2020-09-09 19:49:10 +0000

    guacamoleb: Update to Coral's Sept 2020 fingerprint

--- aosp_guacamoleb.mk
+++ aosp_guacamoleb.mk
@@ -42,7 +42,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 PRODUCT_CHARACTERISTICS := nosdcard
 
 # Build info
-BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus7 \
     PRODUCT_NAME=OnePlus7 \
